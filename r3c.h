0eeaa0b65b7c63245eaefeb8549d3637535a4246
eyjian <eyjian@live.com>
2016-09-09 10:59:09 +0800

    修复中文乱码问题

--- r3c.h
+++ r3c.h
@@ -49,16 +49,16 @@
 #define PRINT_COLOR_BLUE         "\033[0;32;34m"
 #define PRINT_COLOR_GREEN        "\033[0;32;32m"
 #define PRINT_COLOR_WHITE        "\033[1;37m"
-#define PRINT_COLOR_CYAN         "\033[0;36m"      // ����ɫ, ��ɫ
-#define PRINT_COLOR_PURPLE       "\033[0;35m"      // ��ɫ
-#define PRINT_COLOR_BROWN        "\033[0;33m"      // ��ɫ, ��ɫ
-#define PRINT_COLOR_DARY_GRAY    "\033[1;30m"      // �����ɫ
+#define PRINT_COLOR_CYAN         "\033[0;36m"
+#define PRINT_COLOR_PURPLE       "\033[0;35m"
+#define PRINT_COLOR_BROWN        "\033[0;33m"
+#define PRINT_COLOR_DARY_GRAY    "\033[1;30m"
 #define PRINT_COLOR_LIGHT_RED    "\033[1;31m"
 #define PRINT_COLOR_LIGHT_GREEN  "\033[1;32m"
 #define PRINT_COLOR_LIGHT_BLUE   "\033[1;34m"
 #define PRINT_COLOR_LIGHT_CYAN   "\033[1;36m"
 #define PRINT_COLOR_LIGHT_PURPLE "\033[1;35m"
-#define PRINT_COLOR_LIGHT_GRAY   "\033[0;37m"      // ����ɫ
+#define PRINT_COLOR_LIGHT_GRAY   "\033[0;37m"
 namespace r3c {
 
 enum ZADDFLAG
