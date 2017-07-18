32e75a308746fa3127bae2fa7ea41bd58a9dda9d
eyjian <eyjian@live.com>
2017-07-18 18:56:27 +0800

    增加有关eval错误的说明： ERR Error running script (call to f_9b8bd9adab23a1c5fb1a5142b577a0e7b4cb166d): @user_script:1: @user_script: 1: Lua script attempted to access a non local key in a cluster node

--- r3c.cpp
+++ r3c.cpp
@@ -1928,11 +1928,12 @@ const redisReply* CRedisClient::redis_command(int excepted_reply_type, std::pair
                 freeReplyObject(redis_reply);
                 redis_reply = NULL;
 
-                // CLUSTERDOWN The cluster is down (while master is down) （需重试错误）
-                // WRONGTYPE Operation against a key holding the wrong kind of value （此种错误时不需重试）
-                // LOADING Redis is loading the dataset in memory （需重试错误）
-                // MOVED 6474 127.0.0.1:6380 （需重试错误）
+                // CLUSTERDOWN The cluster is down (while master is down)（需重试错误）
+                // WRONGTYPE Operation against a key holding the wrong kind of value（此种错误时不需重试）
+                // LOADING Redis is loading the dataset in memory（需重试错误）
+                // MOVED 6474 127.0.0.1:6380（需重试错误）
                 // ERR wrong number of arguments for 'sadd' command|(0)（此种错误时不需重试）
+                // ERR Error running script (call to f_9b8bd9adab23a1c5fb1a5142b577a0e7b4cb166d): @user_script:1: @user_script: 1: Lua script attempted to access a non local key in a cluster node（需重试错误）
                 // NOSCRIPT No matching script. Please use EVAL.
                 (*g_error_log)("[%s:%d][%d/%d][%s][%s:%d](%d)%s|(%d)%s\n", __FILE__, __LINE__, i, _retry_times, command, node.first.c_str(), node.second, errcode, errmsg.c_str(), redis_context->err, redis_context->errstr);
                 if ((0 == strncmp(errmsg.c_str(), "WRONGTYPE", sizeof("WRONGTYPE")-1)) ||
