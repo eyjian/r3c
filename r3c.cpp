27e407fbb46f7f9603fc2e56a80bd28797c69ccd
eyjian <eyjian@live.com>
2018-08-08 14:04:32 +0800

    remove get_data/set_data

--- r3c.cpp
+++ r3c.cpp
@@ -2440,12 +2440,6 @@ void CRedisClient::init()
         {
             init_cluster();
         }
-
-        // initialize `_data`
-        for (size_t i=0; i<sizeof(_data)/sizeof(_data[0]); ++i)
-        {
-            _data[i] = NULL;
-        }
     }
     catch (...)
     {
