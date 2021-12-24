67da450f83a87bf755012bd962b083640d6bc84d
eyjian <eyjian@live.com>
2021-12-24 23:09:59 +0800

    Add hsetex,hsetnxex,hmincrby based on eval

--- r3c_helper.h
+++ r3c_helper.h
@@ -33,4 +33,55 @@ inline int lpop(CRedisClient* redis, const std::string& key, std::vector<std::st
     return 0;
 }
 
+inline bool hsetex(CRedisClient* redis, const std::string& key, const std::string& field, const std::string& value, uint32_t expired_seconds, Node* which=NULL, int num_retries=0)
+{
+    const std::string lua_scripts =
+            "local n;n=redis.call('HSET',KEYS[1],ARGV[1],ARGV[2]);"
+            "if (n>0) then redis.call('EXPIRE',KEYS[1],ARGV[3]) end;return n;";
+    std::vector<std::string> parameters(3);
+    parameters[0] = field;
+    parameters[1] = value;
+    parameters[2] = int2string(expired_seconds);
+    const RedisReplyHelper redis_reply = redis->eval(key, lua_scripts, parameters, which, num_retries);
+    if (REDIS_REPLY_INTEGER == redis_reply->type)
+        return 1 == redis_reply->integer;
+    return true;
+}
+
+inline bool hsetnxex(CRedisClient* redis, const std::string& key, const std::string& field, const std::string& value, uint32_t expired_seconds, Node* which=NULL, int num_retries=0)
+{
+    const std::string lua_scripts =
+            "local n=redis.call('HLEN',KEYS[1]);"
+            "local m=redis.call('HSETNX',KEYS[1],ARGV[1],ARGV[2]);"
+            "if(n==0) then redis.call('EXPIRE',KEYS[1],ARGV[3]) end;return m;";
+    std::vector<std::string> parameters(3);
+    parameters[0] = field;
+    parameters[1] = value;
+    parameters[2] = int2string(expired_seconds);
+    const RedisReplyHelper redis_reply = redis->eval(key, lua_scripts, parameters, which, num_retries);
+    if (REDIS_REPLY_INTEGER == redis_reply->type)
+        return 1 == redis_reply->integer;
+    return true;
+}
+
+inline void hmincrby(CRedisClient* redis, const std::string& key, const std::vector<std::pair<std::string, int64_t> >& increments, std::vector<int64_t>* newvalues, Node* which=NULL, int num_retries=0)
+{
+    const std::string lua_scripts =
+            "local j=1;local results={};"
+            "for i=1,#ARGV,2 do local f=ARGV[i];"
+            "local v=ARGV[i+1];"
+            "results[j]=redis.call('HINCRBY',KEYS[1],f,v);j=j+1; end;"
+            "return results;";
+    std::vector<std::string> parameters(2*increments.size());
+    for (std::vector<std::pair<std::string, int64_t> >::size_type i=0,j=0; i<increments.size(); ++i,j+=2)
+    {
+        const std::pair<std::string, int64_t>& increment = increments[i];
+        parameters[j] = increment.first;
+        parameters[j+1] = int2string(increment.second);
+    }
+    const RedisReplyHelper redis_reply = redis->eval(key, lua_scripts, parameters, which, num_retries);
+    if (REDIS_REPLY_ARRAY == redis_reply->type)
+        get_values(redis_reply.get(), newvalues);
+}
+
 } // namespace r3c {
