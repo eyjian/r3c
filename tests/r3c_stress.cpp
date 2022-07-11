3583957d4fbcc9bce0b0edf7ad9497f61f81c7f3
leolaxie <leolaxie@tencent.com>
2022-07-11 18:57:40 +0800

    enhance: fix r3c_stress_hash with pw arg

--- tests/r3c_stress.cpp
+++ tests/r3c_stress.cpp
@@ -8,64 +8,64 @@
 
 
 // argv[1] redis nodes
-// argv[2] number of keys
-// argv[3] number of cycles
-int main(int argc, char* argv[])
+// argc[2] redis password
+// argv[3] number of keys
+// argv[4] number of cycles
+int main(int argc, char *argv[])
 {
-    if (argc != 4)
+    if (argc != 5)
     {
-        fprintf(stderr, "Usage: %s <redis nodes> <number of keys> <number of cycles>\n", argv[0]);
-        fprintf(stderr, "Example: %s 192.168.1.61:6379,192.168.1.62:6379 100 1000000\n", argv[0]);
+        fprintf(stderr, "Usage: %s <redis nodes> <password> <number of fields> <number of cycles>\n", argv[0]);
+        fprintf(stderr, "Example: %s 127.0.0.1:6379,127.0.0.1:6380 123456 10 100000\n", argv[0]);
         exit(1);
-    }
-    else
+    } else
     {
         const int num_retries = r3c::NUM_RETRIES;
-        const uint32_t expired_seconds = 3600*24;
-        const std::string& redis_nodes = argv[1];
-        const int num_keys = atoi(argv[2]);
-        const int num_cycles = atoi(argv[3]);
-        r3c::CRedisClient redis(redis_nodes);
+        const uint32_t expired_seconds = 3600 * 24;
+        const std::string &redis_nodes = argv[1];
+        const std::string &redis_pw = argv[2];
+        const int num_keys = atoi(argv[3]);
+        const int num_cycles = atoi(argv[4]);
+        r3c::CRedisClient redis(redis_nodes, redis_pw);
         std::vector<std::string> keys(num_keys);
 
         try
         {
-            for (std::vector<std::string>::size_type j=0; j<keys.size(); ++j)
+            for (std::vector<std::string>::size_type j = 0; j < keys.size(); ++j)
             {
                 char key_buf[64];
                 snprintf(key_buf, sizeof(key_buf), "KEY_%zd", j);
                 keys[j] = key_buf;
                 redis.del(keys[j]);
             }
-            for (int i=0; i<num_cycles; ++i)
+            for (int i = 0; i < num_cycles; ++i)
             {
                 if ((i > 0) && (0 == i % 10000))
                 {
                     fprintf(stdout, "%d\n", i);
                 }
-                for (std::vector<std::string>::size_type j=0; j<keys.size(); ++j)
+                for (std::vector<std::string>::size_type j = 0; j < keys.size(); ++j)
                 {
-                    const std::string& key = keys[j];
+                    const std::string &key = keys[j];
                     redis.setex(key, "1", expired_seconds, NULL, num_retries);
                 }
             }
 
-            for (std::vector<std::string>::size_type j=0; j<keys.size(); ++j)
+            for (std::vector<std::string>::size_type j = 0; j < keys.size(); ++j)
             {
-                const std::string& key = keys[j];
+                const std::string &key = keys[j];
                 std::string value;
                 if (redis.get(key, &value))
                 {
                     fprintf(stdout, "%s => %s\n", key.c_str(), value.c_str());
-                }
-                else
+                } else
                 {
                     fprintf(stdout, "%s => %s\n", key.c_str(), "NONE");
                 }
             }
             return 0;
         }
-        catch (r3c::CRedisException& ex)
+        catch (r3c::CRedisException &ex)
         {
             fprintf(stderr, "%s\n", ex.str().c_str());
             exit(1);
--- tests/r3c_stress_hash.cpp
+++ tests/r3c_stress_hash.cpp
@@ -8,13 +8,44 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <vector>
+#include <queue>
 
 #define MAX(a, b) (a) > (b) ? (a) : (b)
 #define MIN(a, b) (a) < (b) ? (a) : (b)
+static const int num_top = 10; // 统计top10的查询时间
 
-typedef void (*CB)(r3c::CRedisClient &redis);
+// Top
+class Topk
+{
+public:
+    Topk(int k) : _k(k) {}
 
-static void fill_cb(CB cbs[]);  // todo: multi test functionality
+    void insert(uint64_t x)
+    {
+        if (_q.size() < (uint64_t) _k)
+        { _q.push(x); }
+        else if (x > _q.top())
+        {
+            _q.pop();
+            _q.push(x);
+        }
+    }
+
+    std::vector<uint64_t> finalize()
+    {
+        std::vector<uint64_t> result(_q.size());
+        while (_q.size())
+        {
+            result[_q.size() - 1] = _q.top();
+            _q.pop();
+        }
+        return result;
+    }
+
+private:
+    int _k;
+    std::priority_queue<uint64_t, std::vector<uint64_t>, std::greater<uint64_t>> _q;
+};
 
 // 统计
 typedef struct redisStat
@@ -25,12 +56,14 @@ typedef struct redisStat
     uint64_t min_us;
     uint64_t max_us;
     uint64_t sum_us;
+    Topk top;
 
     redisStat(const std::string &name, const int numFields, const int numCycles) : name(name), num_fields(numFields),
-                                                                                   num_cycles(numCycles), min_us(0),
-                                                                                   max_us(0), sum_us(0) {}
+                                                                                   num_cycles(numCycles),
+                                                                                   min_us(999999L),
+                                                                                   max_us(0), sum_us(0), top(num_top) {}
 
-    friend std::ostream &operator<<(FILE *os, const redisStat &stat)
+    friend std::ostream &operator<<(FILE *os, redisStat &stat)
     {
         fprintf(os, "test name: %s\n", stat.name.c_str());
         fprintf(os, "number of fields: %u\n", stat.num_fields);
@@ -38,61 +71,115 @@ typedef struct redisStat
         fprintf(os, "avg time cost: %lu us\n", stat.sum_us / stat.num_cycles);
         fprintf(os, "max time cost: %lu us\n", stat.max_us);
         fprintf(os, "min time cost: %lu us\n", stat.min_us);
+        fprintf(os, "top%d:\n", num_top);
+        auto res = stat.top.finalize();
+        for (int i = 0; i < num_top; ++i)
+        {
+            fprintf(os, "%lu\t", res[i]);
+        }
+        fprintf(os, "\n\n");
     }
 } redisStat;
 
+/// test hget
+// HGET all fields
+void test_hget(r3c::CRedisClient &redis_, redisStat &stat_)
+{
+    CStopWatch the_timer;
+    std::vector<std::string> fields(stat_.num_fields);
+    volatile uint64_t span;
+
+    try
+    {
+        redis_.hsetnx(stat_.name, "", ""); // init
+        for (std::vector<std::string>::size_type j = 0; j < fields.size(); ++j)
+        {
+            char field_buf[32]; // fixed field size
+            snprintf(field_buf, sizeof(field_buf), "FIELD_%zd", j);
+            fields[j] = field_buf;
+            redis_.hset(stat_.name, fields[j], fields[j]);
+        }
+        the_timer.restart(); // init
+        for (int j = 0; j < stat_.num_cycles; ++j)
+        {
+            std::map<std::string, std::string> dumped;
+            redis_.hget(stat_.name, fields, &dumped);
+            span = the_timer.get_elapsed_microseconds();
+            stat_.min_us = MIN(stat_.min_us, span);
+            stat_.max_us = MAX(stat_.max_us, span);
+            stat_.sum_us += span;
+            stat_.top.insert(span);
+        }
+    }
+    catch (r3c::CRedisException &ex)
+    {
+        fprintf(stderr, "%s\n", ex.str().c_str());
+        exit(1);
+    }
+    stdout << stat_;
+}
+
+
+/// test hgetall
+// HGETALL all fields
+void test_hgetall(r3c::CRedisClient &redis_, redisStat &stat_)
+{
+    CStopWatch the_timer;
+    std::vector<std::string> fields(stat_.num_fields);
+    volatile uint64_t span;
+
+    try
+    {
+        redis_.hsetnx(stat_.name, "", ""); // init
+        for (std::vector<std::string>::size_type j = 0; j < fields.size(); ++j)
+        {
+            char field_buf[32]; // fixed field size
+            snprintf(field_buf, sizeof(field_buf), "FIELD_%zd", j);
+            fields[j] = field_buf;
+            redis_.hset(stat_.name, fields[j], fields[j]);
+        }
+        the_timer.restart(); // init
+        for (int j = 0; j < stat_.num_cycles; ++j)
+        {
+            std::map<std::string, std::string> dumped;
+            redis_.hgetall(stat_.name, &dumped);
+            span = the_timer.get_elapsed_microseconds();
+            stat_.min_us = MIN(stat_.min_us, span);
+            stat_.max_us = MAX(stat_.max_us, span);
+            stat_.sum_us += span;
+            stat_.top.insert(span);
+        }
+    }
+    catch (r3c::CRedisException &ex)
+    {
+        fprintf(stderr, "%s\n", ex.str().c_str());
+        exit(1);
+    }
+    stdout << stat_;
+}
 
 // argv[1] redis nodes
-// argv[2] number of fields
-// argv[3] number of cycles
+// argc[2] redis password
+// argv[3] number of fields
+// argv[4] number of cycles
 int main(int argc, char *argv[])
 {
-    if (argc != 4)
+    if (argc != 5)
     {
-        fprintf(stderr, "Usage: %s <redis nodes> <number of fields> <number of cycles>\n", argv[0]);
-        fprintf(stderr, "Example: %s 127.0.0.1:6379,127.0.0.1:6380 10 100000\n", argv[0]);
+        fprintf(stderr, "Usage: %s <redis nodes> <password> <number of fields> <number of cycles>\n", argv[0]);
+        fprintf(stderr, "Example: %s 127.0.0.1:6379,127.0.0.1:6380 123456 10 100000\n", argv[0]);
         exit(1);
     } else
     {
         const int num_retries = r3c::NUM_RETRIES;
         const uint32_t expired_seconds = 3600 * 24;
         const std::string &redis_nodes = argv[1];
-        const std::string the_key = "stress_hget";
-        redisStat stat(the_key, atoi(argv[2]), atoi(argv[3]));
-        r3c::CRedisClient the_redis(redis_nodes);
-        CStopWatch the_timer;
-        std::vector<std::string> fields(stat.num_fields);
-        volatile uint64_t span;
-
-        try
-        {
-            the_redis.hsetnx(the_key, "", ""); // init
-            for (std::vector<std::string>::size_type j = 0; j < fields.size(); ++j)
-            {
-                char field_buf[32]; // fixed field size
-                snprintf(field_buf, sizeof(field_buf), "FIELD_%zd", j);
-                fields[j] = field_buf;
-                the_redis.hset(the_key, fields[j], fields[j]);
-            }
-            the_timer.restart(); // init
-            for (int j = 0; j < stat.num_cycles; ++j)
-            {
-                std::string val; // dumped
-                the_redis.hget(the_key, fields[j % stat.num_fields], &val);
-                span = the_timer.get_elapsed_microseconds();
-                stat.min_us = MIN(stat.min_us, span);
-                stat.max_us = MAX(stat.max_us, span);
-                stat.sum_us += span;
-            }
-        }
-        catch (r3c::CRedisException &ex)
-        {
-            fprintf(stderr, "%s\n", ex.str().c_str());
-            exit(1);
-        }
-        stdout << stat;
+        const std::string &redis_pw = argv[2];
+        r3c::CRedisClient the_redis(redis_nodes, redis_pw);
+        // test which
+        redisStat redisStat1("test_hget", atoi(argv[3]), atoi(argv[4]));
+        test_hget(the_redis, redisStat1);
+        redisStat redisStat2("test_hgetall", atoi(argv[3]), atoi(argv[4]));
+        test_hgetall(the_redis, redisStat2);
     }
 }
-
-
-
